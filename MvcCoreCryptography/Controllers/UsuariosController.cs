44dc72d1a49b4f39932254f083a37b54d6259dcd
JoseAntonioLP03 <l.p.joseantonio03@gmail.com>
2026-02-23 14:23:41 +0100

    Add project files.

---
+++ MvcCoreCryptography/Controllers/UsuariosController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreCryptography.Models;
+using MvcCoreCryptography.Repositories;
+
+namespace MvcCoreCryptography.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private RepositoryUsuarios repo;
+
+        public UsuariosController(RepositoryUsuarios repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(string nombre , string email , string imagen , string password)
+        {
+            await this.repo.RegisterUserAsync(nombre,email,imagen,password);
+            ViewData["MENSAJE"] = "Usuario en el sistema";
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> LogIn(string email , string password)
+        {
+            Usuario user = await this.repo.LogInUserAsync( email, password);
+            if(User == null)
+            {
+                ViewData["MENSAJE"] = "Credenciales incorrectas";
+                return View();
+            }
+            else
+            {
+                return View(user);
+            }
+        }
+    }
+}
---
+++ MvcCoreCryptography/Repositories/RepositoryUsuarios.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreCryptography.Data;
+using MvcCoreCryptography.Helpers;
+using MvcCoreCryptography.Models;
+
+namespace MvcCoreCryptography.Repositories
+{
+    public class RepositoryUsuarios
+    {
+        private UsuariosContext context;
+
+        public RepositoryUsuarios(UsuariosContext context)
+        {
+            this.context = context;
+        }
+
+        private async Task<int> GetMaxIdUsuarioAsync()
+        {
+            if(this.context.Usuario.Count() == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return await this.context.Usuario.MaxAsync(z => z.IdUsuario) + 1;
+            }
+        }
+
+        public async Task RegisterUserAsync(string nombre , string email , string imagen , string password)
+        {
+            Usuario user = new Usuario();
+            user.IdUsuario = await this.GetMaxIdUsuarioAsync();
+            user.Nombre = nombre;
+            user.Imagen = imagen;
+            user.Email = email;
+
+            user.Salt = HelperTools.GenerateSalt();
+            user.Password = HelperCryptography.EncryptPassword(password, user.Salt);
+            await this.context.Usuario.AddAsync(user);
+            await this.context.SaveChangesAsync();
+
+        }
+
+        public async Task<Usuario> LogInUserAsync(string email , string password)
+        {
+            var consulta = from datos in this.context.Usuario
+                           where datos.Email == email
+                           select datos;
+            Usuario user =  await consulta.FirstOrDefaultAsync();
+            if(user == null)
+            {
+                return null;
+            }
+            else
+            {
+                string salt = user.Salt;
+                byte[] temp = HelperCryptography.EncryptPassword(password, salt);
+                byte[] passBytes = user.Password;
+                bool response = HelperTools.CompareArrays(temp, passBytes);
+                if(response == true)
+                {
+                    return user;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
